293ff16274e16b091161a966ee1c469934923bf3
Sergiy-Glookh <sglookh@gmail.com>
2023-06-23 00:25:58 +0300

    Update cli_virtual_assistant.py

--- cli_virtual_assistant.py
+++ cli_virtual_assistant.py
@@ -1,10 +1,87 @@
 from collections import UserDict
 
 
+class Field:
+    """Base class for fields in a record."""
+
+    def __init__(self, value: str) -> None:
+        """
+        Initialize a new field.
+
+        Args:
+            value (str): The value of the field.
+        """
+
+        self.value = value
+
+
+class Name(Field):
+    """Name field in a record."""
+
+
+class Phone(Field):
+    """Phone number field in a record."""
+
+
+class Record:
+    """Record representing a contact in the address book."""
+
+    def __init__(self, name: Name, phone: Phone = None) -> None:
+        """
+        Initialize a new record.
+
+        Args:
+            name (Name): The name of the contact.
+            phone (Phone, optional): The phone number of the contact. Defaults to None.
+        """
+
+        self.name = name
+        self.phones = []
+        if phone:        
+            self.phones.append(phone)
+
+    def add_phone(self, phone: Phone) -> None:
+        """
+        Add a phone number to the record.
+
+        Args:
+            phone (Phone): The phone number to add.
+        """
+
+        self.phones.append(phone)
+
+    def remove_phone(self, phone: Phone) -> None:
+        """
+        Remove a phone number from the record.
+
+        Args:
+            phone (Phone): The phone number to remove.
+        """
+
+        for existing_phone in self.phones:
+            if phone.value == existing_phone.value:
+                self.phones.remove(existing_phone)
+
+
+    def edit_phone(self, old_phone: Phone, new_phone: Phone) -> None:
+        """
+        Edit a phone number in the record.
+
+        Args:
+            old_phone (Phone): The old phone number to replace.
+            new_phone (Phone): The new phone number.
+        """
+
+
+        for idx, phone in enumerate(self.phones):
+            if old_phone.value == phone.value:
+                self.phones[idx] = new_phone
+
+
 class AddressBook(UserDict):
     """Address book that extends UserDict."""
 
-    def add_record(self, record: 'Record') -> None:
+    def add_record(self, record: Record) -> None:
         """
         Add a record to the address book.
 
@@ -42,83 +119,12 @@ class AddressBook(UserDict):
         return results
 
 
-class Record:
-    """Record representing a contact in the address book."""
-
-    def __init__(self, name: 'Name', phone: 'Phone' = None) -> None:
-        """
-        Initialize a new record.
-
-        Args:
-            name (Name): The name of the contact.
-            phone (Phone, optional): The phone number of the contact. Defaults to None.
-        """
-
-        self.name = name
-        self.phones = []
-        if phone:        
-            self.phones.append(phone)
-
-    def add_phone(self, phone: 'Phone') -> None:
-        """
-        Add a phone number to the record.
-
-        Args:
-            phone (Phone): The phone number to add.
-        """
-
-        self.phones.append(phone)
-
-    def remove_phone(self, phone: 'Phone') -> None:
-        """
-        Remove a phone number from the record.
-
-        Args:
-            phone (Phone): The phone number to remove.
-        """
-
-        for existing_phone in self.phones:
-            if phone.value == existing_phone.value:
-                self.phones.remove(existing_phone)
-
-
-    def edit_phone(self, old_phone: 'Phone', new_phone: 'Phone') -> None:
-        """
-        Edit a phone number in the record.
-
-        Args:
-            old_phone (Phone): The old phone number to replace.
-            new_phone (Phone): The new phone number.
-        """
-
-
-        for idx, phone in enumerate(self.phones):
-            if old_phone.value == phone.value:
-                self.phones[idx] = new_phone
 
 
 
 
-class Field:
-    """Base class for fields in a record."""
-
-    def __init__(self, value: str) -> None:
-        """
-        Initialize a new field.
-
-        Args:
-            value (str): The value of the field.
-        """
-
-        self.value = value
 
 
-class Name(Field):
-    """Name field in a record."""
-
-
-class Phone(Field):
-    """Phone number field in a record."""
 
 
   
